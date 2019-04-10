18c5be0fc0ea2834e1d439db2bb2b09153611d33
paofan <>
2019-04-10 20:03:14 +0800

    【feature/cll_spider_osx】flask框架

--- test.py
+++ test.py
@@ -1,19 +1,41 @@
+from flask import Flask
+import json
+from flask import abort
+from flask import make_response
+
+app = Flask(__name__)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/todo/list/<int:task_id>', methods=['GET'])
+def index(task_id):
+    ts = [task for task in tasks if task['id'] == task_id]
+    if len(ts) == 0:
+        abort(404)
+    return json.dumps(ts[0])
+
+
+@app.errorhandler(404)
+def not_found(error):
+    print(error)
+    return make_response(json.dumps({'error':'Not Found'}), 404)
+
+
 if __name__ == '__main__':
-    row1 = [1, 2, 3]
-    row2 = [4, 5, 6]
-    row3 = []
-    row1.extend(row2)
-    print(row1)
-    print(row1+row2)
-    row1.append(row2)
-    print(row1)
-    a = [[1,2,3],[4,5,6]]
-    b = a.copy()
-    c = []
-    print(b)
-    a.extend(b)
-    print(a)
-    c.extend(a)
-    c.extend(b)
-    print(c)
+    app.run(debug=True, host='0.0.0.0', port='5000')
 
