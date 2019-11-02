cb2db7932f4c16f7854e6c09fabb0ef5288cc318
Licardo <zhanghao.326159@163.com>
2019-11-02 17:13:52 +0800

    增加杨申淼入口 修改地址

--- ysm.py
+++ ysm.py
@@ -1,5 +1,7 @@
 from db.db_operate import DbOperator
+from bmob.operate_data import OperateData
 from business_gongzhonghao.spider_gzh import SpiderGZH
+
 
 def yangshenmiao():
     # 杨申淼的公众号
@@ -9,5 +11,15 @@ def yangshenmiao():
     DbOperator.insert(spider.handle_datas(spider.get_all_article()))
 
 
+def database_bmob(start, end):
+    ope = OperateData()
+    ope.insert(ope.get_datas(start, end))
+
+
 if __name__ == '__main__':
+    start = DbOperator.query_size()
+    print(str(start))
     yangshenmiao()
+    end = DbOperator.query_size()
+    print(str(start) + '===' + str(end))
+    database_bmob(start, end)
