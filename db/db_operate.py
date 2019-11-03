a6514bc033f3898c320e03a9501f2959cf6e7fb6
paofan <>
2019-11-03 11:20:55 +0800

    【feature/cll_spider_osx】fix 插入数据库的id问题

--- db/db_operate.py
+++ db/db_operate.py
@@ -7,15 +7,15 @@ class DbOperator:
     def insert(datas):
         db = db_base.DbBase.connect()
         cursor = db.cursor()
-        index = DbOperator.query_size(cursor)
+        # index = DbOperator.query_size(cursor)
         for data in datas:
             if DbOperator.find_data_for_url(cursor, data.url) == 0:
                 # 如果数据库中不存在该条数据，执行插入操作
                 # try:
-                index += 1
-                sql = 'insert into cll (id, title, sub_title, url, image, image_urls, description, source, platform, ' \
+                # index += 1
+                sql = 'insert into cll (title, sub_title, url, image, image_urls, description, source, platform, ' \
                       'level, top, type, send_time) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s ,%s) '
-                cursor.execute(sql, (index, data.title, data.sub_title, data.url, data.image,
+                cursor.execute(sql, (data.title, data.sub_title, data.url, data.image,
                                      data.image_urls, data.description, data.source, data.platform,
                                      data.level, data.top, data.type, data.send_time))
                 db.commit()
