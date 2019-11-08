176a1ba6d69b1251de874c96e0815628a1b8e9b1
paofan <>
2019-11-08 23:29:50 +0800

    【feature/cll_spider_osx】fix 定时执行任务

--- main.py
+++ main.py
@@ -92,9 +92,23 @@ if __name__ == '__main__':
 
     # 定时服务 每天每小时执行一次
     schedule = BlockingScheduler()
+
+    # 1.coalesce：当由于某种原因导致某个job积攒了好几次没有实际运行（比如说系统挂了5分钟后恢复，有一个任务是每分钟跑一次的，
+    # 按道理说这5分钟内本来是“计划”运行5次的，但实际没有执行），如果coalesce为True，下次这个job被submit给executor时，只会执行1次，
+    # 也就是最后这次，如果为False，那么会执行5次（不一定，因为还有其他条件，看后面misfire_grace_time的解释）
+    # 2.max_instance: 就是说同一个job同一时间最多有几个实例再跑，比如一个耗时10分钟的job，被指定每分钟运行1次，
+    # 如果我们max_instance值为5，那么在第6~10分钟上，新的运行实例不会被执行，因为已经有5个实例在跑了
+    # 3.misfire_grace_time：设想和上述coalesce类似的场景，如果一个job本来14:00有一次执行，但是由于某种原因没有被调度上，
+    # 现在14:01了，这个14:00的运行实例被提交时，会检查它预订运行的时间和当下时间的差值（这里是1分钟），大于我们设置的30秒限制，
+    # 那么这个运行实例不会被执行。
+    job_defaults = {
+        'coalesce': True,
+        'max_instances': 3,
+        'misfire_grace_time': 300
+    }
     # 每天16-18点的0-4分钟执行 执行频率是5秒钟
     # schedule.add_job(func=Main.execute, trigger='cron', month='1-12', day='1-31', hour='*/12')
-    schedule.add_job(func=Main.execute, trigger='cron', hour='12', minute='30')
+    schedule.add_job(func=Main.execute, job_defaults=job_defaults, trigger='cron', hour='12', minute='30')
     # 每五秒执行一次
     # schedule.add_job(func=loop_data, args=(1,), trigger='interval', seconds=5)
     schedule.start()
